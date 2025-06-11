ff6499d0d55ed577818fdc9d22a7ee4fc20a3ebc
aulil99 <aulilmushalli@gmail.com>
2025-06-11 14:17:52 +0700

    Setting AdminLTE

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		// $this->load->view('layouts/header');
+        // $this->load->view('layouts/sidebar');
+		// $this->load->view('index');
+        // $this->load->view('layouts/footer');
 	}
 }
